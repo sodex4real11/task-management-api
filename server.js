98ef60009a749b714e669d99406ff1089b57e30d
Sodex4real11 <sodex4real11@gmail.com>
2024-07-25 12:43:53 +0700

    new

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const User = require('./models/User');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 dotenv.config();
 
